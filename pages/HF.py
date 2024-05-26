b63562b788b07756076904ea3fda60f39c71ac62
161spok <136721685+161spok@users.noreply.github.com>
2024-05-26 20:27:49 +0200

    Update HF.py

--- pages/HF.py
+++ pages/HF.py
@@ -27,16 +27,5 @@ generate = flan_t5_model.generate([query1, query2])
 #print(generate.generations)
 
 st.write(generate.generations)
-"""
-with st.status("Downloading data...", expanded=True) as status:
-    st.write("Searching for data...")
-    time.sleep(2)
-    st.write("Found URL.")
-    time.sleep(1)
-    st.write("Downloading data...")
-    time.sleep(1)
-    status.update(label="Download complete!", state="complete", expanded=False)
 
-st.write(generate.generations)
-"""
     
